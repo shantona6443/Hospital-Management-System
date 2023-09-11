3f81d7a5120d295970fc288e6fa721976d2448a0
shantona5403 <shantonaakter2089@gmail.com>
2023-09-11 15:20:13 +0600

    Hospital Management System

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// $('.gallery_parts').slick({
+//     centerMode: true,
+//     infinite: true,
+//     arrows:false,
+//     autoplay: true,
+//     dots: true,
+//     centerPadding: '0px',
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '60px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+
+
+$('.counter').counterUp({
+	delay: 10,
+	time: 1000
+});
